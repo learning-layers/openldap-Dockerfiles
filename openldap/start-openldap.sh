5f3581b186d143c3e8a8a27cd34bd7a7ff607f52
istvank <koren@dbis.rwth-aachen.de>
2015-11-12 18:25:58 +0100

    Updated OpenLDAP and configured script
    Now it only tries to put the settings on the first run.

---
+++ openldap/start-openldap.sh
@@ -0,0 +1,13 @@
+if [ -f openldap_configured.txt ];
+then
+   echo "OpenLDAP was already configured, starting it."
+   /usr/local/libexec/slapd -d
+else
+   echo "OpenLDAP was not yet configured. Doing it now and then starting it."
+   /usr/local/libexec/slapd
+   sleep 5
+   ldapadd -x -w $LDAP_ROOT_PASSWORD -D "cn=LayersManager,dc=learning-layers,dc=eu" -f /usr/local/etc/openldap/ldap-init.ldif
+   pkill slapd
+   echo 'OpenLDAP' > openldap_configured.txt
+   /usr/local/libexec/slapd -d
+fi
