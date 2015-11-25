506bc7e477f33211a23973d67e8a26128fa9c365
istvank <koren@dbis.rwth-aachen.de>
2015-11-25 18:07:45 +0100

    Add exec to startup script

--- openldap/start-openldap.sh
+++ openldap/start-openldap.sh
@@ -3,7 +3,7 @@
 if [ -f openldap_configured.txt ];
 then
    echo "OpenLDAP was already configured, starting it."
-   /usr/local/libexec/slapd -d 8
+   exec /usr/local/libexec/slapd -d 8
 else
    echo "OpenLDAP was not yet configured. Doing it now and then starting it."
    /usr/local/libexec/slapd
@@ -11,5 +11,5 @@ else
    ldapadd -x -w $LDAP_ROOT_PASSWORD -D "cn=LayersManager,dc=learning-layers,dc=eu" -f /usr/local/etc/openldap/ldap-init.ldif
    pkill slapd
    echo 'OpenLDAP' > openldap_configured.txt
-   /usr/local/libexec/slapd -d 8
+   exec /usr/local/libexec/slapd -d 8
 fi
